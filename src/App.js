4b5341efe295a6a1d02928cc0ba6eea5f44430dd
minami-cs <mina9301@gmail.com>
2021-07-06 11:13:16 +0900

    modified app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  // 함수형 컴포넌트와 클래스형 컴포넌트 숨기는 버튼 만들기
+  // 기본적으로 보이게 할 것이므로 true값을 준다.
+  var [funcShow, setFuncShow] = useState(true);
+  var [classShow, setClassShow] = useState(true);
   return (
     <div className="container">
       <h1>Hello World!</h1>
-      <FuncComp initNumber={2}></FuncComp>
-      <ClassComp initNumber={2}></ClassComp>
+      <input type="button" value="remove func" onClick={function() {
+        setFuncShow(false);
+      }}></input>
+      <input type="button" value="remove class" onClick={function() {
+        setClassShow(false);
+      }}></input>
+      {funcShow ? <FuncComp initNumber={2}></FuncComp> : null}
+      {classShow ? <ClassComp initNumber={2}></ClassComp> : null}
     </div>
   );
 }
@@ -32,13 +42,38 @@ function FuncComp(props) {
   // side effect(useEffect)는 이미 렌더링된 후에 값을 변경하는 것
   // useEffect는 여러 개 설치 가능
   useEffect(function() {
-    console.log('%cfunc => useEffect (componentDidMount & componentDidUpdate) A' + (++funcId), funcStyle);
-    document.title = number + ' : ' + _date;
-  });
+    console.log('%cfunc => useEffect (componentDidMount) A' + (++funcId), funcStyle);
+    document.title = number;
+    // useEffect가 한번 실행된 후 다시 실행될 때 이전에 실행했던 값을 정리해주는 것이 clean up
+    // 예를 들면 입장할 때 구독하고 퇴장할 때 구독취소를 하는 것
+    // 이 effect는 부모 컴포넌트에서 이 함수 컴포넌트를 삭제하는 경우에 clean-up(unmount)을 하게 된다.
+    return function() {
+      console.log('%cfunc => useEffect return (componentWillUnmount) B' + (++funcId), funcStyle);
+    }
+  // 빈 배열을 넣으면 처음 한 번만 실행되고 그 후로는 실행되지 않는다.(componentDidMount만 하도록)
+  }, []);
+
   useEffect(function() {
-    console.log('%cfunc => useEffect (componentDidMount & componentDidUpdate) B' + (++funcId), funcStyle);
-    document.title = number + ' : ' + _date;
-  });
+    console.log('%cfunc => useEffect number (componentDidMount & componentDidUpdate) A' + (++funcId), funcStyle);
+    document.title = number;
+    // useEffect가 한번 실행된 후 다시 실행될 때 이전에 실행했던 값을 정리해주는 것이 clean up
+    // 예를 들면 입장할 때 구독하고 퇴장할 때 구독취소를 하는 것
+    return function() {
+      console.log('%cfunc => useEffect number return (componentDidMount & componentDidUpdate) B' + (++funcId), funcStyle);
+    }
+    // number만 변경된 것을 감지
+  }, [number]);
+
+  useEffect(function() {
+    console.log('%cfunc => useEffect _date (componentDidMount & componentDidUpdate) A' + (++funcId), funcStyle);
+    document.title = _date;
+    // useEffect가 한번 실행된 후 다시 실행될 때 이전에 실행했던 값을 정리해주는 것이 clean up
+    // 예를 들면 입장할 때 구독하고 퇴장할 때 구독취소를 하는 것
+    return function() {
+      console.log('%cfunc => useEffect _date return (componentDidMount & componentDidUpdate) B' + (++funcId), funcStyle);
+    }
+    // _date만 변경된 것을 감지
+  }, [_date]);
 
   // useState()를 쓰면 무조건 2개의 값으로 이루어진 배열이 리턴됨
   // console.log('numberState', numberState);
@@ -82,6 +117,9 @@ class ClassComp extends React.Component {
   }
   componentDidUpdate(nextProps, nextState) {
     console.log('%cclass => componentDidUpdate',classStyle);
+  }
+  componentWillUnmount() {
+    console.log('%cclass => componentWillUnmount',classStyle);
   }
   render() {
     console.log('%cclass => render',classStyle);
